2aa5099bead0ae4a87ae717ae123ce3852b5a5fe
sh3ngsh3ng <yshenglim13@gmail.com>
2021-09-14 19:41:27 +0000

    get token working, can't get routing

--- 07-onemap-routing/routing.js
+++ 07-onemap-routing/routing.js
@@ -1,5 +1,22 @@
 let oneMap_API_BASE_URL = "https://developers.onemap.sg"
 
+
+// let settings = {
+//     "url"  : oneMap_API_BASE_URL + "/privateapi/auth/post/getToken", 
+//     "data" : 
+//             {
+//                 "email": "leeweixg2001@yahoo.com", 
+//                 "password": "Scatyim777"
+//             }, 
+//     "async" : "true"
+// }
+
+// $.post(settings).done(function(response) {
+//     console.log(response)
+// })
+
+
+
 async function getToken() {
     var settings = {
         "url"  : oneMap_API_BASE_URL + "/privateapi/auth/post/getToken", 
@@ -12,23 +29,23 @@ async function getToken() {
     }
 
     let response = await $.post(settings)
-    let newToken = response.access_token
-    return newToken //returns a promise
+    return response.access_token
 }
 
+let x = getToken()
 
 
 async function getRouting() {
-    let newToken = getToken()
     let response = await axios.get(oneMap_API_BASE_URL + "/privateapi/routingsvc/route", {
         params: {
             'start': "1.3328572,103.74355220000007",
             'end':  "1.32283828324684,103.936051543997",
             'routeType': "walk",
-            'token': 'newToeyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjc5M…BlIn0.BhaTlFrA5vRDhcepdrXTkd9cB9gJpNOzxiDKUYeXWNwken',
+            'token': x,
         }
     })
-    console.log("response")
-    return response.data
+    console.log(response)
+    return "done"
 }
 
+getRouting()
