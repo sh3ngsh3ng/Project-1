ea3dcd5794d70cd12000806b2f525f2cf3cfb86c
Yong Sheng <yshenglim13@gmail.com>
2021-09-23 19:40:29 +0800

    added alternative to jquery

--- 12-phase4part2/script.js
+++ 12-phase4part2/script.js
@@ -112,9 +112,11 @@ document.querySelector("#mode-switch").addEventListener("click", function() {
         document.querySelector("#switch-label").innerHTML = "Mode: Search"
         document.querySelector("#search-bar-div").classList.remove("search-bar-up")
         document.querySelector("#search-bar-div").classList.add("search-bar-show")
-        // filter move up
         document.querySelector("#recommend-btn-div").classList.remove("reco-btn-show")
         document.querySelector("#recommend-btn-div").classList.add("reco-btn-left")
+        $("#filter-btn").hide()
+        // alternatively:
+        // document.querySelector("#filter-btn").style.display = "none"
 
     }
     if (mode) {
@@ -124,7 +126,8 @@ document.querySelector("#mode-switch").addEventListener("click", function() {
         document.querySelector("#recommend-btn-div").classList.remove("reco-btn-left")
         document.querySelector("#recommend-btn-div").classList.add("reco-btn-show")
         $("#filter-btn").show()
-        // document.querySelector("#filter-btn").removeAttribute("display")
+        // alternatively:
+        // document.querySelector("#filter-btn").style.display = ""
     }
 })
 
